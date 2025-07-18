c403cf9a8a8edcc2203d4c78c9423a11064de473
Majo202ios <mariajosesenges2021@gmail.com>
2025-07-17 23:33:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit, join_room
 import eventlet
-
 eventlet.monkey_patch()
+
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -110,7 +110,4 @@ def on_salir(data):
     emit('mensaje', f'{username} salió de la sala.', room=room)
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
-
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
